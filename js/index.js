2d6f6ba4fbdaeecdaac97d7ab7e803e385730399
WretchedEraser <bhawthor@engr-131-02.eastern.ewu.edu>
2015-11-30 16:23:13 -0800

    moved to 100px big nav

--- js/index.js
+++ js/index.js
@@ -3,44 +3,44 @@
 	
 
 
-if (window.matchMedia("(min-width: 700px)").matches) {
-        console.log("if");
-		// window width is at least 500px
-        $(document).ready( function() {
-  
-  // On click, remove class on active element, add it to the new one
-  
-  $('header nav a').click( function(e) {
-    
-    $('header nav a.active').removeClass('active');
-    $(this).addClass('active');
-    
-    // Scroll to anchor
-    
-    $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - 60},'500');
-    
-    e.preventDefault();
-    return false;
-    
-  });
-  
-  // On scroll, remove class on active element and add it to the new one
-  
-  $(document).scroll(function() {
-     
-     var position = Math.floor($(this).scrollTop() / 1000) + 1;
-    
-     $('header nav a.active').removeClass('active');
-     $('header nav a.link-' + position).addClass('active');
-    
-  });
-  
-});
-	}
-	else {
-		// window width is less than 500px
-        console.log("else");
-        $(document).ready( function() {
+//if (window.matchMedia("(min-width: 700px)").matches) {
+//        console.log("if");
+//		// window width is at least 500px
+//        $(document).ready( function() {
+//  
+//  // On click, remove class on active element, add it to the new one
+//  
+//  $('header nav a').click( function(e) {
+//    
+//    $('header nav a.active').removeClass('active');
+//    $(this).addClass('active');
+//    
+//    // Scroll to anchor
+//    
+//    $('html,body').animate({scrollTop: $($(this).attr('href')).offset().top - 60},'500');
+//    
+//    e.preventDefault();
+//    return false;
+//    
+//  });
+//  
+//  // On scroll, remove class on active element and add it to the new one
+//  
+//  $(document).scroll(function() {
+//     
+//     var position = Math.floor($(this).scrollTop() / 1000) + 1;
+//    
+//     $('header nav a.active').removeClass('active');
+//     $('header nav a.link-' + position).addClass('active');
+//    
+//  });
+//  
+//});
+//	}
+//	else {
+//		// window width is less than 500px
+//        console.log("else");
+       $(document).ready( function() {
   
   // On click, remove class on active element, add it to the new one
   
@@ -71,5 +71,5 @@ if (window.matchMedia("(min-width: 700px)").matches) {
   
 });
         
-	}
+//	}
 
