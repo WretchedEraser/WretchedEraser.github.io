a112023089e4d218d5c6ca5d1cb32c227863514c
WretchedEraser <brendanhawthorne@Brendans-MacBook-Pro.local>
2015-12-01 10:28:42 -0800

    fixed big nav to 100px

--- js/index.js
+++ js/index.js
@@ -55,21 +55,18 @@
     
     e.preventDefault();
     return false;
-    
   });
-  
   // On scroll, remove class on active element and add it to the new one
-  
+
   $(document).scroll(function() {
-     
+
      var position = Math.floor($(this).scrollTop() / 1000) + 1;
-    
+
      $('header nav a.active').removeClass('active');
      $('header nav a.link-' + position).addClass('active');
-    
+
   });
-  
+
 });
-        
 //	}
 
